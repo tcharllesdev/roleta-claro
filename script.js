c7dfd94b99ccb6ba5c39d289d150b99efc06ca7f
Tcharlles <tcharlles3@gmail.com>
2024-05-21 19:10:29 -0300

    fixes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const roleta = document.querySelector(".roleta");
 const popup = document.querySelector(".popup");
 const rouletteAudio = document.getElementById("rouletteAudio");
 const rouletteSuccess = document.getElementById("rouletteSuccess");
+const aviso = document.querySelector(".warning");
 
 function removerGiroClasses() {
   roleta.classList.remove("giro1", "giro2", "giro3", "giro4");
@@ -11,14 +12,25 @@ function removerGiroClasses() {
 btn.addEventListener("click", () => {
   removerGiroClasses();
 
-  const randomGiro = Math.floor(Math.random() * 4) + 1;
-
-  roleta.classList.add(`giro${randomGiro}`);
-  rouletteAudio.play();
+  //const randomGiro = Math.floor(Math.random() * 4) + 1;
 
   btn.style.display = "none";
+  roleta.classList.add("giro1");
+  rouletteAudio.play();
+  aviso.style.display = "block";
+
   setTimeout(() => {
-    popup.style.display = "flex";
-    rouletteSuccess.play();
-  }, 14500);
+    removerGiroClasses();
+    roleta.classList.add("giro2");
+    rouletteAudio.play();
+    setTimeout(() => {
+      popup.style.display = "flex";
+      rouletteSuccess.play();
+    }, 12000);
+  }, 16000);
+
+  // setTimeout(() => {
+  //   popup.style.display = "flex";
+  //   rouletteSuccess.play();
+  // }, 14500);
 });
