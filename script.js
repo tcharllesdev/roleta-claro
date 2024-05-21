29ac662f4d395a75caaf038b5fb418a0e6739e10
Tcharlles <tcharlles3@gmail.com>
2024-05-21 10:40:33 -0300

    frufru

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector(".btn");
 const roleta = document.querySelector(".roleta");
 const popup = document.querySelector(".popup");
+const rouletteAudio = document.getElementById("rouletteAudio");
+const rouletteSuccess = document.getElementById("rouletteSuccess");
 
 function removerGiroClasses() {
   roleta.classList.remove("giro1", "giro2", "giro3", "giro4");
@@ -12,9 +14,11 @@ btn.addEventListener("click", () => {
   const randomGiro = Math.floor(Math.random() * 4) + 1;
 
   roleta.classList.add(`giro${randomGiro}`);
+  rouletteAudio.play();
 
   btn.style.display = "none";
   setTimeout(() => {
     popup.style.display = "flex";
-  }, 6500);
+    rouletteSuccess.play();
+  }, 12500);
 });
